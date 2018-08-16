fbbdb7a6751558423fe50df88c5103c4f7980f30
Thomas Carli <thomascarli@gmail.com>
2018-08-16 10:05:07 +0200

    external-openstack-cloud-controller-manager

---
+++ modules/install-k8s/cloud-config.sh
@@ -0,0 +1,11 @@
+cat <<EOF
+[Global]
+auth-url=https://auth.cloud.ovh.net/v2.0/
+username=<myUserName>
+password=<myPassword>
+region=<myRegion>
+tenant-name=<myTenant>
+
+[Networking]
+ipv6-support-disabled=true
+EOF
